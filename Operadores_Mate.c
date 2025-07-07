4968d2617a0e183ba2b87759f3b4773671864883
Leyze Martins <120027405+LeyzeMartins@users.noreply.github.com>
2025-07-07 19:02:51 +0000

    Operadores_Matematicos

---
+++ Operadores_Mate.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+// Soma (+)
+// Subtração (-)
+// Multiplicação (*)
+// Divisão (/)
+
+// Declaração das Variaveis;
+
+int numero1,numero2;
+
+int soma,subtracao, multiplicacao, divisao;
+
+// Operação soma
+soma = numero1 + numero2;
+
+// Operação subtração 
+subtracao = numero1 - numero2;
+
+// Operação multiplicação
+multiplicacao = numero1 * numero2;
+
+// Operação divisão
+divisao = numero1 / numero2;
+
+}
+
+
