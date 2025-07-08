8c459b0d1fc34e266bb3401da14b7310472f8a2c
Leyze Martins <120027405+LeyzeMartins@users.noreply.github.com>
2025-07-08 14:03:42 +0000

    Incremento_Decremento.c

---
+++ Incremento_decremento.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+/*
+Incremento (++)
+Pré-Incremento (++a)
+Pós-Incremento (a++)
+Decremento (--)
+Pré-Decremento (--a)
+Pós-Decremnto (a--)
+*/
+
+int numero1 = 1;
+
+printf("Antes incremento %d\n",numero1);
+
+ numero1++;
+ 
+printf("Apos incremento %d\n",numero1);
+
+
+}
