559c76424f5616409e548cd1b00f2c02de85d977
Leyze Martins <120027405+LeyzeMartins@users.noreply.github.com>
2025-07-08 14:43:40 +0000

    incremento_decremento.c

--- Incremento_decremento.c
+++ Incremento_decremento.c
@@ -1,6 +1,6 @@
-#include <stdio.h>
+#include <stdio.h> // Biblioteca padrão para entrada e saída, usada para funções como printf.
 
-int main(){
+int main(){ // Função principal onde o programa começa a execução.
 
 /*
 Incremento (++)
@@ -11,13 +11,37 @@ Pré-Decremento (--a)
 Pós-Decremnto (a--)
 */
 
-int numero1 = 1;
+//Declarando as Variaveis:
+int numero1 = 1, resultado;
 
-printf("Antes incremento %d\n",numero1);
+printf("Antes incremento: %d\n",numero1);
 
- numero1++;
+//Numero1 = Numero1 + 1
+//numero1 += 1 SERIA SE USASE O OPERADOR DE ATRIBUIÇÃO
+//Pos- incremento
+// resultado - numero1;
+//numero1++
+
+ resultado = numero1++;
  
-printf("Apos incremento %d\n",numero1);
+//printf("Apos incremento: %d\n",numero1);
+printf("Apos  o Pós-incremento - numero1: %d - resultado:%d\n",numero1,resultado);
+//Numero1 = Numero1 - 1
+//numero1 -= 1 SERIA SE USASE O OPERADOR DE ATRIBUIÇÃO
+ //numero1--;
+ 
+resultado = ++numero1;
+ 
+
+printf("Apos  o Pré-incremento - numero1: %d - resultado:%d\n",numero1,resultado);
+//Numero1 = Numero1 - 1
+//numero1 -= 1 SERIA SE USASE O OPERADOR DE ATRIBUIÇÃO
+ //numero1--;
+  
+
+//printf("Apos o Decremento: %d\n",numero1);
+resultado = numero1 --;
+printf("Apos  o Pos-decremento - numero1: %d - resultado:%d\n",numero1,resultado);
 
 
 }
