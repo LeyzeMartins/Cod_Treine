70782fedce1576854149c2f76bd6f9f05084928f
Leyze Martins <120027405+LeyzeMartins@users.noreply.github.com>
2025-07-08 11:03:34 +0000

    Operadores_mate.c

--- Operadores_Mate.c
+++ Operadores_Mate.c
@@ -13,6 +13,12 @@ int numero1,numero2;
 
 int soma,subtracao, multiplicacao, divisao;
 
+printf("Entre com o primerio numero: ");
+scanf("%d", &numero1);
+
+printf("Entre com segundo numero: ");
+scanf("%d", &numero2);
+
 // Operação soma
 soma = numero1 + numero2;
 
@@ -24,6 +30,16 @@ multiplicacao = numero1 * numero2;
 
 // Operação divisão
 divisao = numero1 / numero2;
+
+printf("A soma é: %d\n", soma );
+
+printf("A subtração  é: %d\n", subtracao );
+
+printf("A mutiplicação é: %d\n", multiplicacao );
+
+printf("A divisão: %d\n", divisao );
+
+
 
 }
 
